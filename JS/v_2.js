ce1a4a9c1976fc83dda41a460496e6bebccf4548
Carlos <carlosrt123450@gmail.com>
2025-10-02 13:00:30 +0200

    UPDATE empiezo de nuevo con array de objetos

---
+++ JS/v_2.js
@@ -0,0 +1,12 @@
+let organizaciones = [
+    {id: 0, nombre: "Cruz Roja", precio: 5, interacciones: 0},
+    {id: 1, nombre: "UNICEF", precio: 10, interacciones: 0},
+    {id: 2, nombre: "WWF", precio: 7, interacciones: 0},
+    {id: 3, nombre: "msf", precio: 12, interacciones: 0},
+    {id: 4, nombre: "STC", precio: 8, interacciones: 0},
+    {id: 5, nombre: "Greenpeace", precio: 6, interacciones: 0},
+    {id: 6, nombre: "Cáritas ", precio: 9, interacciones: 0},
+    {id: 7, nombre: "Amnistía ", precio: 11, interacciones: 0},
+    {id: 8, nombre: "FVF ", precio: 4, interacciones: 0},
+    {id: 9, nombre: "Aldeas ", precio: 13, interacciones: 0},
+];
