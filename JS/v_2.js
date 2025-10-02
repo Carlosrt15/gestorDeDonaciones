126738288a7127a6dc8ceb270a9e6e17a6895970
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-10-02 17:03:54 +0200

    UPDATE de la funcion que almacena el total aportado a cada ONGs

--- JS/v_2.js
+++ JS/v_2.js
@@ -1,20 +1,21 @@
 let organizaciones = [
-    {id: 0, nombre: "Cruz Roja", precio: 5, interacciones: 0},
-    {id: 1, nombre: "UNICEF", precio: 10, interacciones: 0},
-    {id: 2, nombre: "WWF", precio: 7, interacciones: 0},
-    {id: 3, nombre: "msf", precio: 12, interacciones: 0},
-    {id: 4, nombre: "STC", precio: 8, interacciones: 0},
-    {id: 5, nombre: "Greenpeace", precio: 6, interacciones: 0},
-    {id: 6, nombre: "Cáritas", precio: 9, interacciones: 0},
-    {id: 7, nombre: "Amnistía" , precio: 11, interacciones: 0},
-    {id: 8, nombre: "FVF" , precio: 4, interacciones: 0},
-    {id: 9, nombre: "Aldeas" , precio: 13, interacciones: 0},
+    {id: 0, nombre: "Cruz Roja", precio: 5, interacciones: 0, total: 0},
+    {id: 1, nombre: "UNICEF", precio: 10, interacciones: 0,total: 0},
+    {id: 2, nombre: "WWF", precio: 7, interacciones: 0,total: 0},
+    {id: 3, nombre: "msf", precio: 12, interacciones: 0,total: 0},
+    {id: 4, nombre: "STC", precio: 8, interacciones: 0,total: 0},
+    {id: 5, nombre: "Greenpeace", precio: 6, interacciones: 0,total: 0},
+    {id: 6, nombre: "Cáritas", precio: 9, interacciones: 0,total: 0},
+    {id: 7, nombre: "Amnistía" , precio: 11, interacciones: 0,total: 0},
+    {id: 8, nombre: "FVF" , precio: 4, interacciones: 0,total: 0},
+    {id: 9, nombre: "Aldeas" , precio: 13, interacciones: 0,total: 0},
 ];
 
 let mostrarHtml = false;
 
 organizaciones.forEach(orgs => {
         let img = document.getElementById(orgs.id);
+
         if(img) {
             img.addEventListener("click", () => contarPulsar(orgs.id));
         }
@@ -23,12 +24,25 @@ organizaciones.forEach(orgs => {
 
 function contarPulsar(id) {
     let orgs = organizaciones[id];
-    if(orgs) {
-        orgs.interacciones++;
-        console.log(orgs.nombre+" clicado "+orgs.interacciones+ " veces");
-    }
 
     
+
+    if(orgs) {
+        orgs.interacciones++;
+        TotalOrgDonado(id);
+        console.log(orgs.nombre+" clicado "+orgs.interacciones+ " veces");
+    }
+       
+
+}
+
+function TotalOrgDonado(id){
+    let orgs = organizaciones[id];
+    
+    if(orgs.interacciones >= 0) {
+        orgs.total = orgs.precio * orgs.interacciones;
+        console.log(orgs.nombre+" Tiene un total de : "+orgs.total);
+    }
 
 }
 
