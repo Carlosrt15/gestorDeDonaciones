3dabff77a553906f0b248269166cc31bf78dea90
Carlos <carlosrt123450@gmail.com>
2025-10-02 13:46:28 +0200

    Create funcion para registrar contador mas asiganar id a imagenes

--- JS/v_2.js
+++ JS/v_2.js
@@ -5,8 +5,33 @@ let organizaciones = [
     {id: 3, nombre: "msf", precio: 12, interacciones: 0},
     {id: 4, nombre: "STC", precio: 8, interacciones: 0},
     {id: 5, nombre: "Greenpeace", precio: 6, interacciones: 0},
-    {id: 6, nombre: "Cáritas ", precio: 9, interacciones: 0},
-    {id: 7, nombre: "Amnistía ", precio: 11, interacciones: 0},
-    {id: 8, nombre: "FVF ", precio: 4, interacciones: 0},
-    {id: 9, nombre: "Aldeas ", precio: 13, interacciones: 0},
+    {id: 6, nombre: "Cáritas", precio: 9, interacciones: 0},
+    {id: 7, nombre: "Amnistía" , precio: 11, interacciones: 0},
+    {id: 8, nombre: "FVF" , precio: 4, interacciones: 0},
+    {id: 9, nombre: "Aldeas" , precio: 13, interacciones: 0},
 ];
+
+let mostrarHtml = false;
+
+organizaciones.forEach(orgs => {
+        let img = document.getElementById(orgs.id);
+        if(img) {
+            img.addEventListener("click", () => contarPulsar(orgs.id));
+        }
+    });
+
+
+function contarPulsar(id) {
+    let orgs = organizaciones[id];
+    if(orgs) {
+        orgs.interacciones++;
+        console.log(orgs.nombre+" clicado "+orgs.interacciones+ " veces");
+    }
+
+    
+
+}
+
+
+
+
